dd2021321fd201c2c37284433b4f3e1cfdea101a
kevin <kevinkolyakov@gmail.com>
2023-12-27 20:56:36 -0500

    fixed bullet collision

--- bullet.gd
+++ bullet.gd
@@ -4,12 +4,16 @@ class_name Bullet
 var speed = 500
 var move_direction: Vector2 = Vector2.ZERO
 var damage
-
 func _process(delta):
 	global_position += move_direction * delta * speed
 
-func _on_body_entered(body):
+func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
 
-func _on_visible_on_screen_notifier_2d_screen_exited():
+func _on_area_entered(area):
+	print("bruh")
+	queue_free()
+
+
+func _on_body_entered(body):
 	queue_free()
